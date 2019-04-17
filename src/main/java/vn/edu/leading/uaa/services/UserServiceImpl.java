12321ee2319dbcc811ef8f6483f403f00467455a
cntt2-tamguyen <nhantam181@gmail.com>
2019-04-17 18:30:13 +0700

    [UAA-6] Add Repository, Service

---
+++ src/main/java/vn/edu/leading/uaa/services/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package vn.edu.leading.uaa.services;
+
+import org.springframework.stereotype.Service;
+import vn.edu.leading.uaa.models.UserModel;
+import vn.edu.leading.uaa.repositories.RoleRepository;
+import vn.edu.leading.uaa.repositories.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public List<UserModel> search(String term) {
+        return userRepository.findAllByUsernameContaining(term);
+    }
+
+    @Override
+    public UserModel findById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public boolean update(UserModel user) {
+        UserModel userModel = userRepository.findById(user.getId()).orElse(null);
+        if (userModel == null)
+            return false;
+        userRepository.save(user);
+        return true;
+    }
+
+    @Override
+    public void save(UserModel user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        UserModel userModel = userRepository.findById(id).orElse(null);
+        if (userModel == null)
+            return false;
+        userRepository.delete(userModel);
+        return true;
+    }
+
+}
